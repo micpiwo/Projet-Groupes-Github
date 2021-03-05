1acbdc519fea20eb06ba6fedf226ed75692e2835
micpiwo <micpiwo@hotmail.fr>
2021-03-05 06:42:17 +0100

    Ici branche mic free lance

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lobster+Two:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="styles.css"/>
+    </head>
+    <body>
+    <div class="container">
+        <h1 class="text-center text-info">Ceci sur la branche main par micpiwo</h1>
+        <p>Ici la branche de mic onlineformapro</p>
+        <p>Un autre paragraphe</p>
+        <b>Un autre ajout</b>
+
+
+        <ul class="list-group">
+            <li class="list-group-item">Ici la branche micfreelance</li>
+            <li class="list-group-item">Un autre test</li>
+        </ul>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="app.js"></script>
+
+    </body>
+</html>
